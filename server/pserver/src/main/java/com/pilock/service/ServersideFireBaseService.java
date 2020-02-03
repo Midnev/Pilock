3c88fa9940b143fb54c00b58aa83909c8ea34fd9
Midnev <40971560+Midnev@users.noreply.github.com>
2020-02-03 11:21:22 +0900

    sensitive data
    checking for leaks

--- server/pserver/src/main/java/com/pilock/service/ServersideFireBaseService.java
+++ server/pserver/src/main/java/com/pilock/service/ServersideFireBaseService.java
@@ -3,7 +3,7 @@ package com.pilock.service;
 import com.google.auth.oauth2.GoogleCredentials;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
-import org.springframework.beans.factory.annotation.Value;
+import com.google.firebase.messaging.*;
 import org.springframework.stereotype.Service;
 
 import java.io.FileInputStream;
@@ -11,32 +11,52 @@ import java.io.FileInputStream;
 @Service
 public class ServersideFireBaseService {
 
-    @Value("${firebase.server-key}")
-    private String serverKey;
-
-    @Value("${firebase.sender-id}")
-    private String senderId;
+    //@Value("${firebase.url}")
+    private String dbUrl = "https://pilock-a4732.firebaseio.com";
+    //@Value("${firebase.path}")
+    private String configPath ="src/main/resources/pilock-firebase-adminsdk.json";
 
     public ServersideFireBaseService() {
-        System.out.println(serverKey);
-    }
-
-    private void initFireBase() {
-
         try {
             FileInputStream serviceAccount =
-                    new FileInputStream("path/to/serviceAccountKey.json");
+                    new FileInputStream(configPath);
 
             FirebaseOptions options = new FirebaseOptions.Builder()
                     .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-                    .setDatabaseUrl("https://pilock-a4732.firebaseio.com")
+                    .setDatabaseUrl(dbUrl)
                     .build();
 
             FirebaseApp.initializeApp(options);
         }catch (Exception e){
             System.out.println(e.getMessage());
         }
-
-
     }
+
+
+    private static final String ANDROID_NEWS_ICON_RESOURCE = "news_alert_icon";
+    private static final int APNS_NEWS_BADGE_RESOURCE = 42;
+    private static final String WEBPUSH_NEWS_ICON_URL = "https://auto.news.url/alert.png";
+
+    public String sendAutoDoorNotification(String title, String body,String topic) throws Exception {
+        Message message = Message.builder()
+                .setNotification(new Notification(title, body))
+                .setAndroidConfig(AndroidConfig.builder()
+                        .setNotification(AndroidNotification.builder()
+                                .setIcon(ANDROID_NEWS_ICON_RESOURCE)
+                                .build())
+                        .build())
+                /*.setApnsConfig(ApnsConfig.builder() //ios
+                        .setAps(Aps.builder()
+                                .setBadge(APNS_NEWS_BADGE_RESOURCE)
+                                .build())
+                        .build())
+                .setWebpushConfig(WebpushConfig.builder() //web
+                        .setNotification(new WebpushNotification("title", title, WEBPUSH_NEWS_ICON_URL))
+                        .build())*/
+                .setTopic(topic)
+                .build();
+        return FirebaseMessaging.getInstance().sendAsync(message).get();
+    }
+
+
 }
